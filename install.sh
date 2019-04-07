e43a6cb3a8581816a391646756c87c4d326daa58
FELIPECAHM <49304834+FELIPECAHM@users.noreply.github.com>
2019-04-07 03:10:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 chmod +x sudo
 cp sudo /data/data/com.termux/files/usr/bin/sudo
-cd /data/data/com.termux/files/home/CAHMSUDO
+cd ;cd CAHMSUDO
 mv sudo /data/data/com.termux/files/usr/bin/applets/sudo
 cd ;rm -rf CAHMSUDO
+clear
+echo ""
 echo "      Se instalo sudo correctamente"
 sudo su
